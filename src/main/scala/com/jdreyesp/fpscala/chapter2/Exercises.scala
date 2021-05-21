320e68f3754450e3a30f613663e5fb0e4994507e
Juan Diego Reyes Ponce <jdiego.reyes@gmail.com>
2021-05-21 13:28:37 +0200

    more collection exercises

--- src/main/scala/com/jdreyesp/fpscala/chapter2/Exercises.scala
+++ src/main/scala/com/jdreyesp/fpscala/chapter2/Exercises.scala
@@ -1,9 +1,12 @@
 package com.jdreyesp.fpscala.chapter2
 
+import scala.annotation.tailrec
+
 object Exercises extends App {
 
   //Functions as literals
 
+  @tailrec
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     if (as.isEmpty || as.size == 1) true
     else if (as.size == 2) ordered(as.head, as.tail.head)
@@ -22,9 +25,24 @@ object Exercises extends App {
 
   println(curry[Int, String, Double]((e1: Int, e2: String) => e1.toDouble + e2.toDouble)(3)("5"))
 
+  def myfunction(a: Int)(b: Int)(c: Int): Int = {
+    a + b + c
+  }
+
   def compose[A, B, C](f: B => C, g: A => B): A => C = {
     (a: A) => f(g(a))
   }
 
+  val mf1: Int => Int => Int = myfunction(1)_
+  val mf2: Int => Int = mf1(2)
+  val myval: Int = mf2(3)
+
+  val myFunction2 = new Function1[Int, Int] {
+    override def apply(v1: Int): Int = v1
+  }
+
+  compose(mf1, myFunction2)
+
+  println(myval)
   println(compose[String, String, String]((s: String) => s + " World", (s: String) => s + " Hello")("John"))
 }
--- src/main/scala/com/jdreyesp/fpscala/chapter3/Exercises.scala
+++ src/main/scala/com/jdreyesp/fpscala/chapter3/Exercises.scala
@@ -1,5 +1,7 @@
 package com.jdreyesp.fpscala.chapter3
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -16,23 +18,15 @@ object List {
     case Cons(x, xs) => x * product(xs)
   }
 
-  def apply[A](as: A*): List[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
-}
-
-object Exercises extends App {
-
-  import List._
-
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y case Cons(h, t) => h + sum(t)
-    case _ => 101
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
   }
 
-  println(x)
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
   def tail[A](l: List[A]): List[A] = {
     l match {
@@ -77,6 +71,66 @@ object Exercises extends App {
     }
   }
 
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((_, b) => b + 1)
+  }
+
+  def reverse[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(head, tail) => foldLeft[A, List[A]](tail, List(head))((acc, elem) => append(List(elem), acc))
+    }
+  }
+
+  def sumFoldLeft(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(head, tail) => foldLeft(tail, head)(_ + _)
+  }
+
+  def productFoldLeft(ints: List[Int]): Int = ints match {
+    case Nil => 1
+    case Cons(head, tail) => foldLeft(tail, head)(_ * _)
+  }
+
+  //Steps for a1=[1,2,3] a2=[4,5,6]
+  //[1,2,3] foldright 3
+  //[1,2,3,4*] foldright 4
+  //[1,2,3,4, 5*] foldright 5
+  //[1,2,3,4,5,6*]
+  def appendFoldRight[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+    case Nil => Nil
+    case elems => foldRight[A, List[A]](elems, a2)(Cons(_, _))
+  }
+
+  //Steps for a1=[1,2,3] a2=[4,5,6]
+  //[3,2,1] //reverse
+  //[4,3,2,1] //foldleft
+  //[5,4,3,2,1] //foldleft
+  //[6,5,4,3,2,1] //foldleft
+  //[1,2,3,4,5,6] //reverse
+  def appendFoldLeft[A](a1: List[A], a2: List[A]): List[A] = a2 match {
+    case Nil => Nil
+    case elems => reverse(foldLeft[A, List[A]](elems, reverse(a1))((acc, elem) => Cons(elem, acc)))
+  }
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
+
+object Exercises extends App {
+
+  import List._
+
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y case Cons(h, t) => h + foldRight(t, 0)(_ + _)
+    case _ => 101
+  }
+
+  println(x)
+
   val l = List(1,2,3,4)
   println(tail(Nil))
   println(tail(l))
@@ -86,4 +140,19 @@ object Exercises extends App {
   println(drop(l, 1))
   println(dropWhile(l, (elem: Int) => elem < 3))
   println(init(l))
+  println(length(l))
+
+  def b(l: List[Int]): Boolean = l match { case Cons(_, _) => true}
+  def b2(l: List[Int]): Boolean = l match {case Cons(_, _) => true}
+
+  println(s"B IS: ${b(l) && b2(l)}")
+
+  //scala.collectionList() Constructors are made with foldRights :)
+  println(foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_)))
+  println(foldLeft(l, 0)(_ + _))
+  println(reverse(l))
+  println(sumFoldLeft(l))
+  println(productFoldLeft(l))
+  println(appendFoldRight(l, List(5,6,7)))
+  println(appendFoldLeft(l, List(5,6,7)))
 }
